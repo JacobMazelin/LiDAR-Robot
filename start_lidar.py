e05ed12f184e9c447a8768751cd32cddd21703bd
Jacob Mazelin <jjmazelin@gmail.com>
2025-06-29 15:08:29 -0400

    LiDAR continuously spinning

--- start_lidar.py
+++ start_lidar.py
@@ -1,22 +1,26 @@
 import serial, time
 
 PORT = '/dev/ttyUSB0'
-BAUD = 115200
-START_CMD = bytes.fromhex('A50000000001')
+BAUD = 256000
+MOVE_CMD = bytes.fromhex('A50000000001')
+READ_CMD = bytes.fromhex('A560')
 
 def main():
-    print("Starting LIDAR...")
-    ser = serial.Serial(port = PORT, baudrate= BAUD, timeout=1)
-    time.sleep(5)
 
-    ser.write(START_CMD)
-    print("Sent start command; waiting for motor...")
+    ser = serial.Serial(PORT, BAUD, timeout=1)
+    time.sleep(1)
+    print(f"Connected to LiDAR on {PORT} at {BAUD} baud.")
+    ser.write(MOVE_CMD)
+    ser.write(READ_CMD) 
+    print("LiDAR spinning... Reading raw data")
 
-    time.sleep(5)
-    resp = ser.read(ser.in_waiting or 1)
-    print("Response:", resp)
-
-    ser.close()
-
+    try:
+        while True:
+            data = ser.read(64)  # read a chunk
+            print(data)           # binary dump
+    except KeyboardInterrupt:
+        ser.close()
+        print("\nStopped reading.")
+        
 if __name__ == '__main__':
     main()
